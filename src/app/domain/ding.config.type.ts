7b3dcc5bc1f1aed5cc22eacc77d73f62d6fb92fe
zhaoyunxing <2385585770@qq.com>
2021-08-26 13:58:42 +0800

    添加注释

--- src/app/domain/ding.config.type.ts
+++ src/app/domain/ding.config.type.ts
@@ -1,9 +1,40 @@
+/**
+ * dd.config中所有的参数必须直接来自服务端
+ */
 class DingConfig {
+
+  /**
+   * 必填，微应用ID
+   */
   agentId?: number
+
+  /**
+   * 必填，企业ID
+   */
   corpId?: string
+
+  /**
+   * 必填，生成签名的时间戳
+   */
   timeStamp?: number
+
+  /**
+   * 必填，自定义固定字符串。
+   */
   nonce?: string
+
+  /**
+   * 必填，签名
+   */
   signature?: string
+
+  /**
+   * 选填。0表示微应用的jsapi,1表示服务窗的jsapi；不填默认为0。该参数从dingtalk.js的0.8.3版本开始支持
+   */
   type?: number
+
+  /**
+   *  需要使用的jsapi列表
+   */
   apis?: []
 }
--- src/app/domain/response.type.ts
+++ src/app/domain/response.type.ts
@@ -1,10 +1,25 @@
+/**
+ * 接口返回
+ */
 class Res<T> {
 
+  /**
+   * msg 消息
+   */
   msg?: string
 
+  /**
+   * code 码
+   */
   code?: number
 
+  /**
+   * 是否成功
+   */
   succeed?: boolean
 
+  /**
+   * 返回数据
+   */
   data?: T
 }
