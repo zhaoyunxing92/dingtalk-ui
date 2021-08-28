cc26f4ed83d0411fa2740ab7367df364538d4914
zhaoyunxing <2385585770@qq.com>
2021-08-28 12:00:14 +0800

    授权界面

--- src/app/pages/mandate/mandate.component.ts
+++ src/app/pages/mandate/mandate.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-mandate',
@@ -6,8 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mandate.component.less']
 })
 export class MandateComponent implements OnInit {
+  /**
+   * 标签
+   */
+  labels: Label[] = []
 
-  constructor() { }
+  constructor() {
+    this.labels = [
+      {
+        name: "获取用户授权",
+        path: "/mandate/user",
+        desc: "钉钉免登相关操作"
+      }, {
+        name: "发单聊消息授权",
+        path: "/mandate/message",
+        desc: "钉钉免登相关操作"
+      }, {
+        name: "授权获取审批实例数据",
+        path: "/mandate/approval",
+        desc: "钉钉免登相关操作"
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
