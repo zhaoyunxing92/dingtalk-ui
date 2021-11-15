709ae52e4aef8cc5451ff045b8914344e2e7a4c6
zhaoyunxing <zhaoyunxing@apache.org>
2021-11-15 13:50:28 +0800

    up:更新包导入

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,7 @@
 import {NgModule} from '@angular/core';
 import {MandateModule} from "./mandate/mandate.module";
 import {WelcomeModule} from "./welcome/welcome.module";
-import {ChatComponent} from './chat/chat.component';
 import {ChatModule} from "./chat/chat.module";
-import {SsoComponent} from './sso/sso.component';
 import {SsoModule} from "./sso/sso.module";
 
 @NgModule({
